269adbdcc926cc0cf7cfac0ef0efe4c6b24d8897
Dimtirije Dragasevic <acczawow94@gmail.com>
2016-08-28 16:51:28 +0200

    first commit

---
+++ crud vezba/frmMain.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace crud_vezba
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ime = new System.Windows.Forms.Label();
+            this.state = new System.Windows.Forms.Label();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.txt_State = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.Location = new System.Drawing.Point(97, 152);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(75, 23);
+            this.btn_Insert.TabIndex = 0;
+            this.btn_Insert.Text = "Insert";
+            this.btn_Insert.UseVisualStyleBackColor = true;
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Location = new System.Drawing.Point(241, 152);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(75, 23);
+            this.btn_Update.TabIndex = 1;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(379, 152);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_Delete.TabIndex = 2;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(168, 206);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ime
+            // 
+            this.ime.AutoSize = true;
+            this.ime.Location = new System.Drawing.Point(64, 47);
+            this.ime.Name = "ime";
+            this.ime.Size = new System.Drawing.Size(35, 13);
+            this.ime.TabIndex = 4;
+            this.ime.Text = "Name";
+            // 
+            // state
+            // 
+            this.state.AutoSize = true;
+            this.state.Location = new System.Drawing.Point(67, 97);
+            this.state.Name = "state";
+            this.state.Size = new System.Drawing.Size(32, 13);
+            this.state.TabIndex = 5;
+            this.state.Text = "State";
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(158, 47);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(100, 20);
+            this.txt_Name.TabIndex = 6;
+            // 
+            // txt_State
+            // 
+            this.txt_State.Location = new System.Drawing.Point(158, 97);
+            this.txt_State.Name = "txt_State";
+            this.txt_State.Size = new System.Drawing.Size(100, 20);
+            this.txt_State.TabIndex = 7;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 368);
+            this.Controls.Add(this.txt_State);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.state);
+            this.Controls.Add(this.ime);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Insert);
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label ime;
+        private System.Windows.Forms.Label state;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.TextBox txt_State;
+    }
+}
+
---
+++ crud vezba/frmMain.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace crud_vezba
+{
+    public partial class frmMain : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\USERS\LENOVO\SOURCE\REPOS\PROJEKATMINIOOP\MALA-VEZBA\CRUD VEZBA\CRUD VEZBA\SAMPLE.MDF;Integrated Security = True");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+        int ID = 0;
+        public frmMain()
+             
+        {
+            InitializeComponent();
+            DisplayData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Insert_Click(object sender, EventArgs e)
+        {
+            if (txt_Name.Text != "" && txt_State.Text != "")
+            {
+                cmd = new SqlCommand("insert into tbl_Record(Name,State) values(@name,@state)", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@name", txt_Name.Text);
+                cmd.Parameters.AddWithValue("@state", txt_State.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record Inserted Successfully");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            }
+        }
+        private void DisplayData()
+        {
+            con.Open();
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("select * from tbl_Record", con);
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+        //Clear Data  
+        private void ClearData()
+        {
+            txt_Name.Text = "";
+            txt_State.Text = "";
+            ID = 0;
+        }
+       
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            if (txt_Name.Text != "" && txt_State.Text != "")
+            {
+                cmd = new SqlCommand("update tbl_Record set Name=@name,State=@state where ID=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", ID);
+                cmd.Parameters.AddWithValue("@name", txt_Name.Text);
+                cmd.Parameters.AddWithValue("@state", txt_State.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record Updated Successfully");
+                con.Close();
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Record to Update");
+            }
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+
+            if (ID != 0)
+            {
+                cmd = new SqlCommand("delete tbl_Record where ID=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", ID);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record Deleted Successfully!");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Record to Delete");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txt_Name.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txt_State.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+        }
+    }
+}
